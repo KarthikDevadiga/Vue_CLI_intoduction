5fdaa84213fb2f1c966cee9a2c4d9d6a5cbcf5b7
Karthik K Devadiga <kdevadiga497@gmail.com>
2021-11-27 11:09:27 +0530

    props

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <hello-world></hello-world>
+    <age :ge="age"></age>
+  </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+import age from "./components/age.vue";
+
 export default {
+  components: { HelloWorld, age },
   name: "App",
+  data() {
+    return {
+      age: 20,
+    };
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/age.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>user's {{ ge }}</p>
+</template>
+
+<script>
+export default {
+  name: "age",
+  props: ["ge"],
+};
+</script>
